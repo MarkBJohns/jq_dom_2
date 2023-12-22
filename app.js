aec90c02635cb2698a1a802a150455ecc197ab06
Mark Johns <mark.b.johns@gmail.com>
2023-12-21 22:35:49 -0500

    finished the assignment

---
+++ app.js
@@ -0,0 +1,62 @@
+$(document).ready(function(){
+    $('#sorting').on('submit', function(e){
+        e.preventDefault();
+        let sortBy = $('#dropdown').val();
+        if (sortBy === 'alpha') {
+            sortByAlpha();
+        } else if (sortBy === 'rating') {
+            sortByRate();
+        }
+    });
+    $('#movies').on('submit', function(e){
+        e.preventDefault();
+        let movieName=$('#movies input[name="movie-name"]').val();
+        let movieRate=parseInt($('#movies input[name="rating"]').val(), 10);
+        let deleteBtn=$('<button/>').text('Delete');
+        if(!isNaN(movieRate)&&movieRate>=1&&movieRate<=10){
+            let $li=$('<li>').append(
+                $('<div>').text(movieName),
+                $('<div>').html('&starf;'.repeat(movieRate)),
+                $('<div>').append(deleteBtn)
+            );
+            $('#name').append($li);
+            $('#movies input:not(.submit)').val('');
+        }else{
+            alert('Please enter a number between 1 and 10 for the rating');
+            throw new Error('Please submit a number between 1 and 10');
+        }
+    });
+});
+
+$(document).ready(function(){
+    $('ol').on('click','button',function(){
+        $(this).closest('li').remove();
+    })
+})
+
+function sortByAlpha(){
+    let $nameList=$('#name');
+    let $lis=$nameList.children('li');
+    $lis.sort(function(a,b){
+        let textA=$(a).find('div:first-child').text().toUpperCase();
+        let textB=$(b).find('div:first-child').text().toUpperCase();
+        return (textA<textB)?-1:(textA>textB)?1:0;
+    });
+    $nameList.html($lis);
+}
+function sortByRate(){
+    let $nameList=$('#name');
+    let $lis=$nameList.children('li');
+    $lis.sort(function(a,b){
+        let ratingA=convertToNumber($(a).find('div:nth-child(2)').html());
+        let ratingB=convertToNumber($(b).find('div:nth-child(2)').html());
+        return ratingB-ratingA;
+    });
+    $nameList.html($lis);
+}
+
+
+function convertToNumber(ratingHtml) {
+    let numericRating=ratingHtml.replace(/&starf;/g,'').length;
+    return numericRating;
+}
